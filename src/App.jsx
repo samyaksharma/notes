37efd7c041c747dbca2d2eb53a02e58676d0884b
Samyak Sharma <samyak200111@gmail.com>
2022-03-28 18:58:54 +0530

    change default count to 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
 
   return (
     <div className="App">
